2e22559fd19cb238856d92049fe7759296eacf5f
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-09-16 14:02:45 -0500

    commit tree 7e7f86bf6d42b0dba2e1ebf6ef12dce8dc8baa7f

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,28 +1,41 @@
-// TODO: Create action creators as defined in tests
-export const addQuote = quote => {
-    return {
-        type: 'ADD_QUOTE',
-        quote: quote
+export default (state = [],action) => {
+    switch (action.type) {
+        case 'ADD_QUOTE':
+            return {...state, quotes: [...state.quotes, action.quote]}
+        case 'REMOVE_QUOTE':
+            const removeQuote = state.quotes.filter(quote => quote.id !== quote.id)
+            return {...state, quotes: removeQuote}  
+    
+        default:
+            return state;
     }
 }
 
-export const removeQuote = quoteId => {
-    return {
-        type: 'REMOVE_QUOTE',
-        quoteId: quoteId
-    }
-}
+// // TODO: Create action creators as defined in tests
+// export const addQuote = quote => {
+//     return {
+//         type: 'ADD_QUOTE',
+//         quote: quote
+//     }
+// }
 
-export const upvoteQuote = quoteId => {
-    return {
-        type: 'UPVOTE_QUOTE',
-        quoteId: quoteId
-    }
-}
+// export const removeQuote = quoteId => {
+//     return {
+//         type: 'REMOVE_QUOTE',
+//         quoteId: quoteId
+//     }
+// }
 
-export const downvoteQuote = quoteId => {
-    return {
-        type: 'DOWNVOTE_QUOTE',
-        quoteId: quoteId
-    }
-}
+// export const upvoteQuote = quoteId => {
+//     return {
+//         type: 'UPVOTE_QUOTE',
+//         quoteId: quoteId
+//     }
+// }
+
+// export const downvoteQuote = quoteId => {
+//     return {
+//         type: 'DOWNVOTE_QUOTE',
+//         quoteId: quoteId
+//     }
+// }
